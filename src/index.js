e7f5ebe77149e7ffda3f0347d8e588bed4bccd94
Hugh A. Miles <hmiles23@ufl.edu>
2017-07-29 17:10:48 -0700

    removed unneed code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import http from 'http';
 import SocketIO from 'socket.io';
-import initializeDb from './Database';
-import config from './config.json';
-import Message from './Message';
 
 const app = express();
 const server = http.Server(app);
 const io = new SocketIO(server);
 const port = process.env.PORT || 3000;
+const db = {}
 
 
 // Start connection with service
@@ -35,19 +33,11 @@ app.get('/', (req, res) => {
   res.sendfile('./public/index.html');
 });
 
-app.get('/channels', (req, res) => {
-  const mQuery = req.query;
-  console.log(mQuery);
-  Message.find(mQuery)
-    .sort('-createdAt')
-    .exec((err, messages) => {
-      if (err) return res.json({ err: 'Problem querying messages' }); // handleError() method should go here based upon message
-      return res.json(messages);
-    });
-});
+// app.get('/channels', (req, res) => {
+//   const mQuery = req.query;
+//   console.log(mQuery);
+// });
 
-initializeDb(config, () => {
-  server.listen(port, () => {
-    console.log(`[INFO] Listening on *: + ${port}`);
-  });
+server.listen(port, () => {
+  console.log(`[INFO] Listening on *: + ${port}`);
 });
